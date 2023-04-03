f9e6a917a48cfe007b6f17f5c1be995666ba5df9
Chanhyeok Choi <82595573+ChanHyeok-Choi@users.noreply.github.com>
2023-04-04 00:20:56 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/ChanHyeok-Choi/CSE364Project.git
 cd CSE364Project
 mvn install
-java -jar target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/cse364-project-1.0-SNAPSHOT.jar
