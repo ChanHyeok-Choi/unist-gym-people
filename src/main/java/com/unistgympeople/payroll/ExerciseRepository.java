2ee9d97166d0cb7573e5eea884f32f05aead8ff3
ntw0926 <38070800+ntw0926@users.noreply.github.com>
2023-04-04 00:23:39 +0900

    Delete ExerciseRepository.java

--- src/main/java/com/unistgympeople/payroll/ExerciseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unistgympeople.payroll;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-
-@Component
-public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
-}
