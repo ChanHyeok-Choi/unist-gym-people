70d5e497cfd761e880b8f28f5efc76925f4f7d9c
ntw0926 <38070800+ntw0926@users.noreply.github.com>
2023-04-06 01:46:17 +0900

    Update User.java

--- src/main/java/com.unistgympeople/model/User.java
+++ src/main/java/com.unistgympeople/model/User.java
@@ -3,9 +3,6 @@ package com.unistgympeople.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Data;
-
-@Data
 @Document(collection = "users")
 public class User {
 
