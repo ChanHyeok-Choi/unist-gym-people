20fee6f95193348886b65a570091824ca1e976f5
chan4184 <chan4184@unist.ac.kr>
2023-04-06 00:24:01 +0900

    Resolve EmployeeRepository.java

--- src/main/java/com.unistgympeople/repository/EmployeeRepository.java
+++ src/main/java/com.unistgympeople/repository/EmployeeRepository.java
@@ -1,6 +1,8 @@
-package com.unistgympeople;
+package com.unistgympeople.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unistgympeople.model.Employee;
 
 interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
