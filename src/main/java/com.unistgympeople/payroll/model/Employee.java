1749f7258904489f1d1f0c8755b65a8492b0d0f6
ntw0926 <ntw0926@unist.ac.kr>
2023-05-05 10:32:32 +0900

    test1

--- src/main/java/com.unistgympeople/payroll/model/Employee.java
+++ src/main/java/com.unistgympeople/payroll/model/Employee.java
@@ -1,9 +1,9 @@
 package com.unistgympeople.payroll.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Objects;
 
 
