df3f04346321a3444e2f840a7da2439055aff69a
ChanHyeok-Choi <chan4184@unist.ac.kr>
2023-05-07 18:58:02 +0900

    Update date format

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ mongod --fork --logpath /var/log/mongodb.log
 mongosh admin --eval "db.createUser({ user: 'admin', pwd: 'password', roles: ['userAdminAnyDatabase'] })"
 
 #Import data/*.csv to MongoDB
-mongoimport --db=cse364 --collection=users --authenticationDatabase admin --username admin --password password --type=csv --file=data/users.csv --fields=userId.int32\(\),timeStamp.Date\(\) --columnsHaveTypes
+mongoimport --db=cse364 --collection=users --authenticationDatabase admin --username admin --password password --type=csv --file=data/users.csv --fields=userId.int32\(\),timeStamp.date\(\) --columnsHaveTypes
 
 mvn package
 java -jar target/cse364-project-1.0-SNAPSHOT.jar
