adce181254a990da61770a2529855c79a52c3bd7
Chanhyeok Choi <82595573+ChanHyeok-Choi@users.noreply.github.com>
2023-05-07 20:15:22 +0900

    Update ApplicationTest.java

--- src/test/java/com/unistgympeople/ApplicationTest.java
+++ src/test/java/com/unistgympeople/ApplicationTest.java
@@ -1,28 +1,13 @@
 package com.unistgympeople;
 
-import com.unistgympeople.realTime.controller.UserController;
-import com.unistgympeople.realTime.model.User;
-import com.unistgympeople.realTime.repository.UserRepository;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
-
-    private UserRepository userRepository;
-    private UserController userController;
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
-    }
-
-    @DisplayName("Test for creating entity")
-    @Test
-    void CreateEntityTest() {
-        User user = new User();
-        ResponseEntity<User> actual = userController.saveUser(user);
     }
 }
