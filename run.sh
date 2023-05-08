6c6b01e0e88b69311548dfa032c32f28a4048b76
Chanhyeok Choi <82595573+ChanHyeok-Choi@users.noreply.github.com>
2023-05-08 16:59:31 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 git clone https://github.com/ChanHyeok-Choi/CSE364Project.git
 cd CSE364Project
 
-#Create User Administrator MongoDB
+# Create User Administrator MongoDB
 mongod --fork --logpath /var/log/mongodb.log
 mongosh admin --eval "db.createUser({ user: 'admin', pwd: 'password', roles: ['userAdminAnyDatabase'] })"
 
-#Import data/*.csv to MongoDB
+# Import data/*.csv to MongoDB
 mongoimport --db=vcse364 --collection=users --authenticationDatabase admin --username admin --password password --type=cs --file=data/users.csv --fields=userId.int32\(\),timeStamp.string\(\) --columnsHaveTypes
 
 mvn package
