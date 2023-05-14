169c5cc1894aa8dc11ac9e7ab08087287c858a9a
YongJun Jeong <dydrkfl0608@unist.ac.kr>
2023-05-15 07:01:29 +0900

    re-change run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/dydrkfl06/CSE364Project.git
+git clone https://github.com/ChanHyeok-Choi/CSE364Project.git
 cd CSE364Project
 
 # Create User Administrator MongoDB
