b7a40585195d7340c8e8802dd064f922b298beb5
YongJun Jeong <101716942+dydrkfl06@users.noreply.github.com>
2023-05-15 22:36:45 +0900

    Update RealTimeWebSocketHandler.java
    deleted unused part

--- src/main/java/com/unistgympeople/realTime/handler/RealTimeWebSocketHandler.java
+++ src/main/java/com/unistgympeople/realTime/handler/RealTimeWebSocketHandler.java
@@ -4,12 +4,9 @@ import com.unistgympeople.realTime.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,28 +16,4 @@ import java.util.List;
 public class RealTimeWebSocketHandler extends TextWebSocketHandler {
     private final UserService userService;
     private List<WebSocketSession> sessions = new ArrayList<>();
-
-    public void updateUserCount() {
-        int userCount = getUserCount();
-        String message = Integer.toString(userCount);
-        for (WebSocketSession session : sessions) {
-            try {
-                session.sendMessage(new TextMessage(message));
-            } catch (IOException e) {
-                log.error("Failed to send WebSocket message to session", e);
-            }
-        }
-    }
-
-    public int getUserCount() { return userService.getUserCount(); }
-
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        sessions.add(session);
-    }
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        sessions.remove(session);
-    }
 }
